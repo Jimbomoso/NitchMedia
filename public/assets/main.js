b9888ce736b1abfd6bc017efef99e46713ca4a84
jimbomoso <jimbomoso@gmail.com>
2020-07-03 08:45:27 -0400

    Adds logos and updates js

---
+++ public/assets/main.js
@@ -0,0 +1,21 @@
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("logo").src = images[x];
+}
+
+// function displayPreviousImage() {
+//     x = (x <= 0) ? images.length - 1 : x - 1;
+//     document.getElementById("logo").src = images[x];
+// }
+
+function startTimer() {
+    setInterval(displayNextImage, 5000);
+}
+
+let images = [], x = -1;
+images[0] = "assets/Logo2.svg";
+images[1] = "assets/Logo3.svg";
+images[2] = "assets/Logo4.svg";
+images[3] = "assets/Logo5.svg";
+images[4] = "assets/Logo.svg";
+
